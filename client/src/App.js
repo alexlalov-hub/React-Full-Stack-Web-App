2050daf8e0d459727844460f253fd476152cc698
Aleksander <alex.lalov04@gmail.com>
2022-07-18 18:36:22 +0300

    Deleting unnecessary images

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
 import { Link, Route } from 'react-router-dom';
-import icon from './4336206.png'
 import './App.css';
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={icon} alt="icon" style={{ color: 'transparent' }}></img>
+
             </header>
         </div>
 
