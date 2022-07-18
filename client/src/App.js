bc587a4cc182186afde969f58dc9b7ec7314ea99
Aleksander <alex.lalov04@gmail.com>
2022-07-18 18:32:32 +0300

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import { Link, Route } from 'react-router-dom';
+import icon from './4336206.png'
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={icon} alt="icon" style={{ color: 'transparent' }}></img>
+            </header>
+        </div>
+
+
+    );
+}
+
+export default App;
