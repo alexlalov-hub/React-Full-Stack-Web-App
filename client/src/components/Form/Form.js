8cd3851c567612ee53c14c96829ebcb83b63949c
Aleksander <alex.lalov04@gmail.com>
2022-07-28 13:17:30 +0300

    First steps of validation

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -16,6 +16,10 @@ const Form = () => {
     const classes = useStyles()
     const dispatch = useDispatch()
 
+    // const onChangeHandler = (e, field) => {
+    //     setPostData({ ...postData, field: e.target.value })
+    // }
+
     const handleSubmit = (e) => {
         e.preventDefault()
 
@@ -30,6 +34,7 @@ const Form = () => {
             <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
                 <Typography variant='h6'>Creating a destination</Typography>
                 <TextField name='creator' variant='outlined' label='Creator' fullWidth value={postData.creator} onChange={(e) => { setPostData({ ...postData, creator: e.target.value }) }} />
+                {/* <Typography variant='h9' color='error' fontFamily='Roboto'>Creator must be at least 3 characters long</Typography> */}
                 <TextField name='title' variant='outlined' label='Title' fullWidth value={postData.title} onChange={(e) => { setPostData({ ...postData, title: e.target.value }) }} />
                 <TextField name='message' variant='outlined' label='Message' fullWidth value={postData.message} onChange={(e) => { setPostData({ ...postData, message: e.target.value }) }} />
                 <TextField name='tags' variant='outlined' label='Tags' fullWidth value={postData.tags} onChange={(e) => { setPostData({ ...postData, tags: e.target.value }) }} />
