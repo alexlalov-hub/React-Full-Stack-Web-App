c8024ed67d77ba5d1fc56e67be8a04abf226364c
Aleksander <alex.lalov04@gmail.com>
2022-08-12 19:23:50 +0300

    Fixed a little typo

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -36,7 +36,7 @@ const signUp = async (req, res) => {
             return res.status(400).json({ message: 'User already exists' })
         }
 
-        if (!password === confirmPassword) {
+        if (password !== confirmPassword) {
             return res.status(404).json({ message: 'Passwords must match' })
         }
 
